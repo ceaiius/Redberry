fd780237a1ab4b6f857480180ea9e71edee71fb6
Nika Mamaladze <n_mamaladze@cu.edu.ge>
2022-03-01 09:09:47 +0400

    finished the first page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+function App(){
+
+    return(
+        <div className="container">
+            <div><h1>Welcome Rocketeer !</h1></div>
+            <div><button>Start Questionnaire</button></div>
+            <div><a><h2>Submitted Applications</h2></a></div>
+            <div><div className="rocketman"></div></div>
+           
+
+           
+        </div>
+    )
+}
+
+export default App;
